f0ec18277f04d589c3504ce21a8a97eab7523286
Luke Kosner <luke.kosner@icloud.com>
2026-02-23 17:40:50 -0500

    Include canvas binaries in pdf-preview function trace

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["@napi-rs/canvas"],
+  outputFileTracingIncludes: {
+    "/handler/pdf-preview": [
+      "./node_modules/@napi-rs/canvas/**/*",
+      "./node_modules/@napi-rs/canvas-*/**/*",
+    ],
+    "/handler/pdf-preview/route": [
+      "./node_modules/@napi-rs/canvas/**/*",
+      "./node_modules/@napi-rs/canvas-*/**/*",
+    ],
+  },
   env: {
     NEXT_PUBLIC_VERCEL_ENV: process.env.VERCEL_ENV
   },
